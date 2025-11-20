b07784d155b3fb314ab2447d309c6c8c5fbe320c
henwh0 <hmcginnis96@gmail.com>
2025-11-20 09:13:16 -0500

    Update ansi_colors.sh

--- ansi_colors.sh
+++ ansi_colors.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
 
-# ANSI COLOR CODES
-RED='\033[31m'
-GREEN='\033[32m'
-YELLOW='\033[33m'
-BLUE='\033[34m'
-MAGENTA='\033[35m'
-NC='\033[0m' # No Color
+# ANSI color codes
+
+# Regular colors
+WHITE=$'\e[0;37m'
+PURPLE=$'\e[0;35m'
+RED=$'\e[0;31m'
+GREEN=$'\e[0;32m'
+YELLOW=$'\e[0;33m'
+BLUE=$'\e[0;34m'
+CYAN=$'\e[0;36m'
+NC=$'\e[0m' # No Color/Default
+
+
+# Bold colors
+BOLD_WHITE=$'\e[1;37m'
+BOLD_PURPLE=$'\e[1;35m'
+BOLD_RED=$'\e[1;31m'
+BOLD_GREEN=$'\e[1;32m'
+BOLD_YELLOW=$'\e[1;33m'
+BOLD_BLUE=$'\e[1;34m'
+BOLD_CYAN=$'\e[1;36m'
+
+
+
+# Underline colors
+UL_WHITE=$'\e[4;37m'
+UL_PURPLE=$'\e[4;35m'
+UL_RED=$'\e[4;31m'
+UL_GREEN=$'\e[4;32m'
+UL_YELLOW=$'\e[4;33m'
+UL_BLUE=$'\e[4;34m'
+UL_CYAN=$'\e[4;36m'
